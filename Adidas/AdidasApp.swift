f8b1dd29fd7e5c66447602a4806ade0dbc53fead
adrianorezena <adrianocr@gmail.com>
2023-02-22 08:28:11 -0300

    Initial Commit

---
+++ Adidas/AdidasApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdidasApp.swift
+//  Adidas
+//
+//  Created by Adriano Rezena on 22/02/23.
+//
+
+import SwiftUI
+
+@main
+struct AdidasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

0fe339008a37ecdf78747396b37a3acd5d24013f
adrianorezena <adrianocr@gmail.com>
2023-02-22 14:00:43 -0300

    add `Image` initializer

---
+++ Adidas/Extensions/Image+Extensions.swift
@@ -0,0 +1,40 @@
+//
+//  View+Extensions.swift
+//  Adidas
+//
+//  Created by Adriano Rezena on 22/02/23.
+//
+
+import SwiftUI
+
+public enum Images: String {
+    
+    enum `Type` {
+        case asset
+        case symbol
+    }
+    
+    case homeMenu = "line.3.horizontal"
+    case homeLogo = "img_adidas_logo"
+    case homeProfile = "img_profile"
+    
+    var type: Type {
+        switch self {
+        case .homeMenu:
+            return .symbol
+        default:
+            return .asset
+        }
+    }
+}
+
+extension Image {
+    public init(_ image: Images) {
+        switch image.type {
+        case .symbol:
+            self.init(systemName: image.rawValue)
+        case .asset:
+            self.init(image.rawValue)
+        }
+    }
+}
--- Adidas/HomeView.swift
+++ Adidas/HomeView.swift
@@ -27,10 +27,33 @@ struct HomeView: View {
 // MARK: - Header
 extension HomeView {
     
+    var headerView: some View {
+        VStack {
+            Spacer()
+            
+            headerMenuProfile
+            
+            Spacer()
+            
+            headerWelcomeText
+
+            Spacer()
+            
+            headerSearchBar
+        }
+        .padding(16)
+        .frame(maxWidth: .infinity)
+        .frame(height: 305)
+        .background(Color.homeHeader)
+        .cornerRadius(32, corners: [.bottomLeft, .bottomRight])
+        .padding(.horizontal, 16)
+    }
+    
     var headerMenuProfile: some View {
         HStack {
             Button(action: {}, label: {
-                Image(systemName: "line.3.horizontal")
+                // Image(systemName: "line.3.horizontal")
+                Image(.homeMenu)
                     .resizable()
                     .renderingMode(.template)
                     .scaledToFit()
@@ -41,14 +64,14 @@ extension HomeView {
 
             Spacer()
 
-            Image("img_adidas_logo")
+            Image(.homeLogo)
                 .resizable()
                 .scaledToFit()
                 .frame(width: 44, height: 44)
 
             Spacer()
 
-            Image("img_profile")
+            Image(.homeProfile)
                 .resizable()
                 .scaledToFit()
                 .clipShape(Circle())
@@ -80,28 +103,6 @@ extension HomeView {
             .padding(16)
             .background(Color.homeSearchBackground)
             .cornerRadius(24)
-    }
-    
-    var headerView: some View {
-        VStack {
-            Spacer()
-            
-            headerMenuProfile
-            
-            Spacer()
-            
-            headerWelcomeText
-
-            Spacer()
-            
-            headerSearchBar
-        }
-        .padding(16)
-        .frame(maxWidth: .infinity)
-        .frame(height: 305)
-        .background(Color.homeHeader)
-        .cornerRadius(32, corners: [.bottomLeft, .bottomRight])
-        .padding(.horizontal, 16)
     }
 }
 

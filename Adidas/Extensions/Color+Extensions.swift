2015f65be0fd2295aeca73801551da673257f310
adrianorezena <adrianocr@gmail.com>
2023-02-22 13:36:27 -0300

    add header view

---
+++ Adidas/Extensions/Color+Extensions.swift
@@ -0,0 +1,13 @@
+//
+//  View+Extensions.swift
+//  Adidas
+//
+//  Created by Adriano Rezena on 22/02/23.
+//
+
+import SwiftUI
+
+extension Color {
+    static let homeHeader: Color = Color(red: 0.7707796693, green: 0.9382742047, blue: 0.9998021722)
+    static let homeSearchBackground: Color = Color.white.opacity(0.8)
+}
--- Adidas/HomeView.swift
+++ Adidas/HomeView.swift
@@ -8,17 +8,104 @@
 import SwiftUI
 
 struct HomeView: View {
+    @State var searchText: String = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Color.black.edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                headerView
+                    .edgesIgnoringSafeArea(.top)
+                
+                Spacer()
+            }
         }
-        .padding()
     }
 }
 
+// MARK: - Header
+extension HomeView {
+    
+    var headerMenuProfile: some View {
+        HStack {
+            Button(action: {}, label: {
+                Image(systemName: "line.3.horizontal")
+                    .resizable()
+                    .renderingMode(.template)
+                    .scaledToFit()
+                    .foregroundColor(.black)
+                    .frame(width: 35, height: 35)
+            })
+            .frame(width: 44, height: 44)
+
+            Spacer()
+
+            Image("img_adidas_logo")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 44, height: 44)
+
+            Spacer()
+
+            Image("img_profile")
+                .resizable()
+                .scaledToFit()
+                .clipShape(Circle())
+                .overlay(
+                    Circle()
+                        .stroke(lineWidth: 1)
+                )
+                .frame(width: 48, height: 48)
+        }
+        .frame(height: 44)
+        .frame(maxWidth: .infinity)
+        
+    }
+    
+    var headerWelcomeText: some View {
+        VStack(alignment: .leading) {
+            Text("Welcome")
+                .font(.title)
+            
+            Text("All united we are our best version")
+                .font(.footnote)
+                .foregroundColor(.gray)
+        }
+        .frame(maxWidth: .infinity, alignment: .leading)
+    }
+    
+    var headerSearchBar: some View {
+        TextField("Search...", text: $searchText)
+            .padding(16)
+            .background(Color.homeSearchBackground)
+            .cornerRadius(24)
+    }
+    
+    var headerView: some View {
+        VStack {
+            Spacer()
+            
+            headerMenuProfile
+            
+            Spacer()
+            
+            headerWelcomeText
+
+            Spacer()
+            
+            headerSearchBar
+        }
+        .padding(16)
+        .frame(maxWidth: .infinity)
+        .frame(height: 305)
+        .background(Color.homeHeader)
+        .cornerRadius(32, corners: [.bottomLeft, .bottomRight])
+        .padding(.horizontal, 16)
+    }
+}
+
+// MARK: - Preview
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
         HomeView()
